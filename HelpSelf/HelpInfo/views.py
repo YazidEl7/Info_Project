305bdbc1d5a684d3f7ea68993f079c9d43c5be45
YazidEl7 <elyazidelhachimi7@gmail.com>
2022-07-06 21:53:34 +0100

    Updating_Still_Ongoing

--- HelpSelf/HelpInfo/views.py
+++ HelpSelf/HelpInfo/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Info
+from django.utils.html import escape
 
 
 # Create your views here.
 
 
 def index(request):
+    return render(request, 'HelpInfo/index.html')
+
+
+def data(request):
     information = Info.objects.all()  # [:5]
     output = '<br>'.join([f"{i.comp.comp_name} {i.user.user} {i.status.status} {i.ip.ip}" for i in information])
-    return HttpResponse(output)
+    output1 = {'Computers': information}
+    return render(request, 'HelpInfo/data.html', output1)
